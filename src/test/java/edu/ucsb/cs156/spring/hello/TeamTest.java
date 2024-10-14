fec0720827195a9d79b17c3e1d76822fc99971b7
salshriaan <salshriaan@ucsb.edu>
2024-10-13 20:06:30 -0700

    SA - equals properly tested

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,6 +1,8 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,6 +21,51 @@ public class TeamTest {
        assert(team.getName().equals("test-team"));
     }
 
+    @Test
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
+    }
+
+    @Test
+    public void equals_correctly_compares_teams() {
+        //check if team is equal to itself
+        Team team1 = new Team("Team 1");
+        team1.addMember("bob");
+        assertTrue(team1.equals(team1), "A team object should be equal to itself.");
+
+        //check if different classes
+        Team team2 = new Team("Team 2");
+        team1.addMember("bob");
+        String notATeam = "Not a Team";
+        assertFalse(team2.equals(notATeam), "A Team should not be equal to an object of a different class.");
+
+        //check if both teams and have same data
+        Team team3 = new Team("Team 3");
+        team3.addMember("bob");
+
+        Team team4 = new Team("Team 3");
+        team4.addMember("bob");
+
+        assertTrue(team3.equals(team4), "Two team objects with the same data should be equal.");
+
+        //check if both teams and have different data
+
+        //same name but different members
+        Team team5 = new Team("Team 5");
+        team5.addMember("bob");
+
+        Team team6 = new Team("Team 5");
+        team6.addMember("foo");
+
+        assertFalse(team5.equals(team6), "Two team objects with different data should not be equal.");
+
+        //same members but different names
+        Team team7 = new Team("Team 7");
+        team7.addMember("bob");
+
+        Team team8 = new Team("Team 8");
+        team8.addMember("bob");
+    }
    
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
